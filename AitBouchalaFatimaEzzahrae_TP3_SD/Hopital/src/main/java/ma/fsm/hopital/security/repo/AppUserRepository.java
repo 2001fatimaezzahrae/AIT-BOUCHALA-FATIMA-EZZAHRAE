8e3a7957dd34299b083e766684a1c23db88875cb
2001fatimaezzahrae <152180866+2001fatimaezzahrae@users.noreply.github.com>
2024-05-11 22:22:04 +0200

    Add files via upload

---
+++ AitBouchalaFatimaEzzahrae_TP3_SD/Hopital/src/main/java/ma/fsm/hopital/security/repo/AppUserRepository.java
@@ -0,0 +1,10 @@
+package ma.fsm.hopital.security.repo;
+
+import ma.fsm.hopital.security.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser,String> {
+    AppUser findByUsername(String username);
+
+
+}
